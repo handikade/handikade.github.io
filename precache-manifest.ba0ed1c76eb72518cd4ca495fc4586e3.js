31334fea0d8c34e716d6ac84933be0865ff0b34f
Dika Handika <dika.csui@gmail.com>
2019-08-21 07:53:22 +0700

    Migrating to React

---
+++ precache-manifest.ba0ed1c76eb72518cd4ca495fc4586e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a666d39ebe1e37a6a03a5d613d3e349",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2402f4c408695a90e54b",
+    "url": "/static/css/main.3765bb04.chunk.css"
+  },
+  {
+    "revision": "f383ca7fbe1f52f7d390",
+    "url": "/static/js/2.16a604e2.chunk.js"
+  },
+  {
+    "revision": "2402f4c408695a90e54b",
+    "url": "/static/js/main.2b50d2c4.chunk.js"
+  },
+  {
+    "revision": "b940c9ac843a29b514c4",
+    "url": "/static/js/runtime~main.660fecb6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
